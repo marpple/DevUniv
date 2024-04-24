8c5b4af0d6b52e6396c26c0608f64cc488db5f90
jeongjaehun <jhj@marpple.com>
2024-04-24 16:50:49 +0900

    feat: 초기 세팅 완료

--- apps/demo/src/pages/HelloWorld/HelloWorld.page.ts
+++ apps/demo/src/pages/HelloWorld/HelloWorld.page.ts
@@ -10,7 +10,7 @@ export class HelloWorldPage extends Page<object> {
         Hello, Rune World <img src="${favicon}" alt="logo" class="${klass.logo}" />
       </div>
       ${new Counter({ count: 0 })}
-      <div class="${klass.title}">
+      <div class="${klass.description}">
         작업을 원하시면 demo 앱을 복사하여 새로운 앱을 추가해서 작업 바랍니다.
       </div>
     </div>`;
